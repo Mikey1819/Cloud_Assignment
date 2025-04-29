53ea522a7f4f8c6b1b390e812b0b7e5fc3942f7e
Mikey <khootengyi@gmail.com>
2025-04-29 22:27:59 +0800

    Change db-name

--- PhpProject1/helper.php
+++ PhpProject1/helper.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'db-cloud.czgog2qqq1ar.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'root');
 define('DB_PASS', 'assignment-pass');
-define('DB_NAME', 'cloud');
+define('DB_NAME', 'db-cloud');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
