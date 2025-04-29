16c3438335dc3c02ca2dd4a1e211bb174454de70
Mikey <khootengyi@gmail.com>
2025-04-29 16:44:58 +0800

    Update helper.php

--- PhpProject1/helper.php
+++ PhpProject1/helper.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', 'assignment-pass');
 define('DB_NAME', 'cloud');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
